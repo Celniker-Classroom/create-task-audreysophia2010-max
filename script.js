1100f9a52ba31a6efc962131f68f1c137d965e11
audreysophia2010-max <audreysophia2010@gmail.com>
2026-04-16 20:39:33 +0000

    Day 4: Finished all HTML and JS! Added incorrect guess logic, win or lose states, equal array procedure, hangman drawing procedure, and got rid of the wrong guess list. Game now functions.

--- script.js
+++ script.js
@@ -1,63 +1,104 @@
-
-// Array of 50 random words
+// Array of 50 random words at least 7 letters long
 words = ["electricity", "hardware", "amphibian", "circumference", "computer", "civilization", "engineering", "hangman", "kangaroo", "imagination", "elasticity", "architecture", "greenhouse", "recursion", "submarine", "earthquake", "resistance", "bioluminescence", "function", "grandfather", "mechanism", "telephone", "bodyguard", "titanic", "mathematics", "approximation", "acidification", "technology", "housekeeper", "universe", "pyramid", "exoskeleton", "membership", "warning", "yourself", "screensaver", "language", "elevator", "tyrannical", "internet", "parameter", "traffic", "encouragement", "consciousness", "basketball", "butterfly", "automatic", "microphone", "cassette", "operation"];
 
 // Game state
 let answer = [];
 let message = [];
-let guess = "";
+let drawCount = 0;
 
 document.getElementById("start").addEventListener("click", function() {
     // Reset everything
     message = [];
-    document.getElementById("head").textContent = "";
-    document.getElementById("body").textContent = "";
-    document.getElementById("legs").textContent = "";
-    document.getElementById("word").textContent = "";
+    drawCount = 0;
+    updateDrawing();
     document.getElementById("guess").value = "";
     document.getElementById("msg").textContent = "Make your first guess!";
+    document.getElementById("submit").disabled = false;
 
     // Choose random word
     let rand = Math.floor(Math.random() * 50);
     answer = words[rand].split("");
-    document.getElementById("msg").textContent = answer.join(""); // debugging purposes
-
     for (i = 0; i < answer.length; i++) {
         message.push("_");
     }
-
     document.getElementById("word").textContent = message.join(" ");
 });
 
+// Guess button logic
 document.getElementById("submit").addEventListener("click", function() {
-    guess = document.getElementById("guess").value.toLowerCase();
-
-    if (guess.length != 1) {
-        document.getElementById("msg").textContent = "Please enter 1 letter.";
-    }
+    let guess = document.getElementById("guess").value.toLowerCase();
 
     if (answer.includes(guess)) {
         // Correct guess
         document.getElementById("msg").textContent = "Correct! The hidden word includes " + guess + "."; 
-
         for (i = 0; i <= answer.length; i++) {
             if (answer[i] === guess) {
                 message[i] = guess;
             }
         }
-
         document.getElementById("word").textContent = message.join(" ");
     } else {
         // Incorrect guess
-        // add to hangman drawing, add to wrong guesses list, tell user to try again in msg
-        document.getElementById("msg").textContent = "The hidden word does not include " + guess + "."; // debugging purposes
-
+        document.getElementById("msg").textContent = "The hidden word does not include " + guess + ".";
+        drawCount++;
+        updateDrawing();
     }
 
+    // Win or lose states
+    if (drawCount === 6) {
+        document.getElementById("msg").textContent = "You lost! The hidden word was " + answer.join("") + ".";
+        document.getElementById("submit").disabled = true;
+    } else if (areEqual(message, answer)) {
+        if (drawCount === 0) {
+            document.getElementById("msg").textContent = "You won! You didn't make any wrong guesses.";
+        } else if (drawCount === 1) {
+            document.getElementById("msg").textContent = "You won! You only made 1 wrong guess.";
+        } else {
+            document.getElementById("msg").textContent = "You won! You only made " + drawCount + " wrong guesses.";
+        }
+        document.getElementById("submit").disabled = true;
+    }
+
+    document.getElementById("guess").value = "";
 });
 
-// make an array for wrong guesses that prints in a list
-// check if a letter has already been guessed
-// add a winning state
-// add a losing state
-// add to the hangman drawing
+// List with iteration, selection, and parameters
+function areEqual(a, b) {
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] != b[i]) return false;
+    }
+    return true;
+}
+
+// Draw function
+function updateDrawing() {
+    if (drawCount === 0) {
+        document.getElementById("head").textContent = "";
+        document.getElementById("body").textContent = "";
+        document.getElementById("legs").textContent = "";
+    } else if (drawCount === 1) {
+        document.getElementById("head").textContent = " O ";
+        document.getElementById("body").textContent = "";
+        document.getElementById("legs").textContent = "";
+    } else if (drawCount === 2) {
+        document.getElementById("head").textContent = " O ";
+        document.getElementById("body").textContent = "/";
+        document.getElementById("legs").textContent = "";
+    } else if (drawCount === 3) {
+        document.getElementById("head").textContent = " O ";
+        document.getElementById("body").textContent = "/|";
+        document.getElementById("legs").textContent = "";
+    } else if (drawCount === 4) {
+        document.getElementById("head").textContent = " O ";
+        document.getElementById("body").textContent = "/|\\";
+        document.getElementById("legs").textContent = "";
+    } else if (drawCount === 5) {
+        document.getElementById("head").textContent = " O ";
+        document.getElementById("body").textContent = "/|\\";
+        document.getElementById("legs").textContent = "/ ";
+    } else if (drawCount === 6) {
+        document.getElementById("head").textContent = " O ";
+        document.getElementById("body").textContent = "/|\\";
+        document.getElementById("legs").textContent = "/ \\";
+    }
+}
